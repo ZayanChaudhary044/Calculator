13dfa85e70771ba87d1b7475ddfa73fc687d2abb
Zayan Chaudhary <ec23095@qmul.ac.uk>
2024-08-06 19:58:26 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Zayan Chaudhary on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
